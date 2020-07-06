385a5fea7d89ae07c152d6cc2f368db58a756236
bwapes <benwaples@gmail.com>
2020-07-06 16:39:02 -0700

    changed phohotos and format

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // import functions and grab DOM elements
-
+const quizButton = document.querySelector('.quizButton')
 // initialize state
  
+quizButton.addEventListener('click', () => {
 // set event listeners to update state and DOM
+    prompt('want to take a quiz?')  
+})
